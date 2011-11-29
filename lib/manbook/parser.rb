d6365b127ea5f8f1d425a87a5ab1cf3e59ee9993
Nicholas E. Rabenau <nerab@gmx.net>
2011-11-29 21:10:55 +0100

    Add TODO for non-trivial HTML pages

--- lib/manbook/parser.rb
+++ lib/manbook/parser.rb
@@ -6,6 +6,10 @@ module ManBook
         # The way we extract the title is highly dependent of the concrete HTML. Yet, I found no other way
         # to extract the title of a man page ín a reliable way.
         #
+        # TODO For git, less, gunzip:
+        # <h2>NAME</h2>
+        # <p style="margin-left:11%; margin-top: 1em">git &minus; the stupid content tracker</p>
+        #
         title = Nokogiri::HTML(File.read(html_file)).xpath("//b[text() = 'NAME']/../following-sibling::p[1]/descendant-or-self::text()").to_s
         {:file_name => File.basename(html_file), :title => title}
       end
