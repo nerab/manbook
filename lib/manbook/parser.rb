74bbb377a6d7703c05e113c143196e0cf3c43368
Nicholas E. Rabenau <nerab@gmx.net>
2011-11-29 23:57:26 +0100

    Create proper NCX and OPF files

--- lib/manbook/parser.rb
+++ lib/manbook/parser.rb
@@ -6,10 +6,6 @@ module ManBook
         # The way we extract the title is highly dependent of the concrete HTML. Yet, I found no other way
         # to extract the title of a man page ín a reliable way.
         #
-        # TODO For git, less, gunzip:
-        # <h2>NAME</h2>
-        # <p style="margin-left:11%; margin-top: 1em">git &minus; the stupid content tracker</p>
-        #
         doc = Nokogiri::HTML(File.read(html_file))
         
         title = doc.xpath("//b[text() = 'NAME']/../following-sibling::p[1]/descendant-or-self::text()").to_s 
@@ -18,7 +14,7 @@ module ManBook
           title = doc.xpath("//h2[text() = 'NAME']/following-sibling::p[1]/descendant-or-self::text()").to_s
         end
         
-        {:file_name => File.basename(html_file), :title => title}
+        {:file_name => File.basename(html_file), :title => title.split("\n").join(' ')}
       end
     end
   end
